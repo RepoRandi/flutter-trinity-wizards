c56340c78ccd08afddafa97c7ecf04b1a5810971
Randi <randimaulanaakbar@gmail.com>
2024-06-22 14:10:12 +0700

    feat: add main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'providers/contact_provider.dart';
+import 'screens/contact_list_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,13 +13,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return ChangeNotifierProvider(
+      create: (context) => ContactProvider()..loadContacts(),
+      child: const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Contact Manager',
+        home: ContactListScreen(),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
