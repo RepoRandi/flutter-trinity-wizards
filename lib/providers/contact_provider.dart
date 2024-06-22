76a3bb9e46cdc56226c13dfd1cfce74a091e9e93
Randi <randimaulanaakbar@gmail.com>
2024-06-22 14:09:30 +0700

    feat: add provider contact

--- lib/providers/contact_provider.dart
+++ lib/providers/contact_provider.dart
@@ -5,13 +5,15 @@ import '../models/contact.dart';
 
 class ContactProvider with ChangeNotifier {
   List<Contact> _contacts = [];
+  List<Contact> _filteredContacts = [];
 
-  List<Contact> get contacts => _contacts;
+  List<Contact> get contacts => _filteredContacts;
 
   Future<void> loadContacts() async {
     final String response = await rootBundle.loadString('assets/data.json');
     final List<dynamic> data = jsonDecode(response);
     _contacts = data.map((json) => Contact.fromJson(json)).toList();
+    _filteredContacts = _contacts;
     notifyListeners();
   }
 
@@ -21,5 +23,24 @@ class ContactProvider with ChangeNotifier {
       _contacts[index] = contact;
       notifyListeners();
     }
+  }
+
+  void addContact(Contact contact) {
+    _contacts.add(contact);
+    _filteredContacts = _contacts;
+    notifyListeners();
+  }
+
+  void searchContacts(String query) {
+    if (query.isEmpty) {
+      _filteredContacts = _contacts;
+    } else {
+      _filteredContacts = _contacts.where((contact) {
+        final fullName =
+            '${contact.firstName} ${contact.lastName}'.toLowerCase();
+        return fullName.contains(query.toLowerCase());
+      }).toList();
+    }
+    notifyListeners();
   }
 }
