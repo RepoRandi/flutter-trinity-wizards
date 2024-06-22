0b64999f238e831022f37918b80da1fca5fc5074
Randi <randimaulanaakbar@gmail.com>
2024-06-22 15:05:07 +0700

    feat: add page add contact screen

--- lib/screens/add_contact_screen.dart
+++ lib/screens/add_contact_screen.dart
@@ -3,7 +3,7 @@ import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import '../models/contact.dart';
 import '../providers/contact_provider.dart';
-import '../themes/color.dart'; //
+import '../themes/color.dart';
 
 class AddContactScreen extends StatefulWidget {
   const AddContactScreen({super.key});
